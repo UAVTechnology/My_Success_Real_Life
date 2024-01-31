bf0e9f28d25fd66c847ec591a7ab3e001b3de480
UAVTechnology <uavtechnology24@gmail.com>
2024-01-31 14:41:44 +0530

    31 Jan 2024

---
+++ src/main/java/com/msrl/mySuccessRealLife/constant/UserConstant.java
@@ -0,0 +1,8 @@
+package com.msrl.mySuccessRealLife.constant;
+
+public class UserConstant {
+	
+	public String msrlId = "1234567891";
+	public String msrlPassword = "1234567";
+	public String userName = "Rajiv Raj";
+}
